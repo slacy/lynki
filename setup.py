977e0bcc9d5484350fe0ff215b0a931b4a7fd5eb
Steve Lacy <slacy@slacy.com>
2012-02-01 14:21:48 -0800

    Fixup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='linky',
+setup(name='lynki',
       version='0.1',
       packages=find_packages(),
       entry_points={
         'console_scripts': [
-            'linky = linky.simple:main',
+            'lynki = lynki.simple:main',
             ],
         },
       )
